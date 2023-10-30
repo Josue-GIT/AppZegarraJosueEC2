5de1dc787257fb008947cf7a4093d9aec748cf92
unknown <jezc170904@gmail.com>
2023-10-30 17:59:53 -0500

    Diseño formulario culminado

--- app/src/main/java/pe/edu/idat/appzegarrajosue/MainActivity.java
+++ app/src/main/java/pe/edu/idat/appzegarrajosue/MainActivity.java
@@ -2,13 +2,34 @@ package pe.edu.idat.appzegarrajosue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import pe.edu.idat.appzegarrajosue.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener{
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnformulario.setOnClickListener(this);
+        binding.btnlistado.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnformulario){
+            startActivity(new Intent(MainActivity.this,
+                    FormularioActivity.class));
+        }else if(v.getId() == R.id.btnlistado){
+            startActivity(new Intent(MainActivity.this,
+                    ListadoActivity.class));
+        }
     }
 }
